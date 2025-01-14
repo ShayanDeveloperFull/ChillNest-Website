5b8fbd03f5670e63e732715f64432f040e623e90
PersianKid100 <shayan_asadpour@hotmail.com>
2025-01-13 19:42:26 -0500

    cors adjust

--- api/index.js
+++ api/index.js
@@ -21,8 +21,9 @@ app.use("/uploads", express.static(__dirname + "/uploads"))
 
 app.use(cors({
   credentials: true,
-  origin: "http://localhost:5173"
+  origin: "https://nestwebsite.onrender.com"
 }));
+
 
 app.use(cookieParser());
 
