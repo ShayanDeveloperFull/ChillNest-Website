f6a837380cef3a36840ad15c8fe383f7c6e536cb
PersianKid100 <shayan_asadpour@hotmail.com>
2025-01-22 01:09:12 -0500

    fix

--- bookingApp/src/App.jsx
+++ bookingApp/src/App.jsx
@@ -13,11 +13,7 @@ import PlacePage from "./pages/PlacePage";
 import BookingPlace from "./pages/BookingPlace";
 import BookingsPage from "./pages/BookingsPage";
 
-if (window.location.hostname === "localhost") {
-  axios.defaults.baseURL = "http://localhost:4000/";
-} else {
-  axios.defaults.baseURL = "https://nestwebsite.onrender.com";
-}
+axios.defaults.baseURL = "https://nestwebsite-backend.onrender.com";
 axios.defaults.withCredentials = true;
 
 function App() {
--- bookingApp/src/pages/PlacesFormPage.jsx
+++ bookingApp/src/pages/PlacesFormPage.jsx
@@ -169,7 +169,7 @@ export default function PlacesFormPage() {
                 <img
                   className="aspect-square w-full object-cover rounded-xl"
                   src={`https://nestwebsite-backend.onrender.com/uploads/${link}`}
-                  alt=""
+                  alt="uploaded"
                 />
                 <button
                   type="button"
