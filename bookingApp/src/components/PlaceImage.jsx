9cf2acdb3add74d857258f620c35acaa121426e2
PersianKid100 <shayan_asadpour@hotmail.com>
2025-01-25 22:35:35 -0500

    fixed pic for usersplacepage and bookingpage

--- bookingApp/src/components/PlaceImage.jsx
+++ bookingApp/src/components/PlaceImage.jsx
@@ -3,13 +3,18 @@ export default function PlaceImage({ place, idx = 0, className = null }) {
     className = "aspect-square object-cover";
   }
 
+  const baseURL =
+    window.location.hostname === "localhost"
+      ? "http://localhost:5000"
+      : "https://nestwebsite-backend.onrender.com";
+
+  const imagePath = place.addedPhotos[idx].startsWith("uploads/")
+    ? place.addedPhotos[idx]
+    : `uploads/${place.addedPhotos[idx]}`;
+
   return (
     <div>
-      <img
-        className={className}
-        src={`https://nestwebsite-server.onrender.com/uploads/${place.addedPhotos[idx]}`}
-        alt=""
-      />
+      <img className={className} src={`${baseURL}/${imagePath}`} alt="" />
     </div>
   );
 }
